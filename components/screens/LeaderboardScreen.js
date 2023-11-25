4982d1871578401a17974a47f4565c8ecf8a8c45
louis <louis.lefebure@student.howest.be>
2023-11-25 16:42:09 +0100

    Connected the app to our hosted backend  + Bearer token + no cors error

--- components/screens/LeaderboardScreen.js
+++ components/screens/LeaderboardScreen.js
@@ -4,7 +4,7 @@ import BottomPlants from '../../assets/images/BottomPlants.png';
 import PlantDetailsScreen from './PlantDetailsScreen';
 import styles from '../../styles/LeaderboardStyles.js';
 
-const URL = 'https://test-plantsense-cms.onrender.com/api/plants?populate=*'; // This is the URL for the Strapi API locally: does not work on mobile ( has to be changed to the IP address of the computer running the Strapi API)
+const URL = 'https://test-plantsense-cms.onrender.com/api/plants?populate=*';
 const TOKEN = '82e80ebaf984fa5045332258aecf63a00059e94bfedd91eed37520c0ce1c57f43f4ba4b0d8cafc342a3c01fbca0c43bc40d35a138edd9cb226a1b9729c6a4697be3193f4db5f710f4f1ad1fafcefdd5183ffec18cac1ee1ef1c4d1f9ff699960fea87f1bb294d702ff075515bb111a494781e88ea574363d6b8e65c22ca99f22';
 
 const LeaderboardScreen = () => {
@@ -12,7 +12,7 @@ const LeaderboardScreen = () => {
 
   const [topUsers, setTopUsers] = useState([]);
 
-  const fetchTopUsers = async () => { // This is the fetch for the Strapi API locally: does not work on mobile ( has to be changed to the IP address of the computer running the Strapi API)
+  const fetchTopUsers = async () => {
       const response = await fetch(URL, {
       headers: {
         Authorization: `Bearer ${TOKEN}`,
