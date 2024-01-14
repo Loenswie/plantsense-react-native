aaf6f8b215641c3447abac9b7446ca00d0d67c65
louis <louis.lefebure@student.howest.be>
2024-01-14 12:38:52 +0100

    Deleted plant id from detail screen

--- components/screens/PlantDetailsScreen.js
+++ components/screens/PlantDetailsScreen.js
@@ -7,7 +7,6 @@ const PlantDetailsScreen = ({plant, onBackClick}) => {
     return (
         <View style={styles.PlantDetailsContainer}>
             <Text style={styles.title}>{plant.attributes.Name}</Text>
-            <Text style={styles.text}>Rank: {plant.id}</Text>
             <Text style={styles.text}>Owner: {plant.attributes.owner.data.attributes.username}</Text>
             <Text style={styles.text}>Streak: {plant.attributes.Streak} 🔥</Text>
             <Image style={styles.image2} source={{uri: plant.attributes.Image.data[0].attributes.url}}/>
